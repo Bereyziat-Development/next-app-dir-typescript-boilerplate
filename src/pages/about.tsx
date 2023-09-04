57ca0a5c1ff55868a0d9f5e50a83363ea776dbdc
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2023-09-04 10:22:38 +0200

    Initial commit

---
+++ src/pages/about.tsx
@@ -0,0 +1,21 @@
+import { Meta } from '@/components/layouts/meta.layout';
+import { MainTemplate } from '@/components/templates/main.template';
+
+const About = () => (
+  <MainTemplate meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
+      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
+      voluptatibus distinctio recusandae autem esse explicabo molestias officia
+      placeat, accusamus aut saepe.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
+      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
+      voluptatibus distinctio recusandae autem esse explicabo molestias officia
+      placeat, accusamus aut saepe.
+    </p>
+  </MainTemplate>
+);
+
+export default About;
